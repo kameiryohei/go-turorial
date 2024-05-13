c2086f2d286b5c307911412c59976ca70dd87e0b
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2024-05-13 22:43:21 +0900

    dbなどを作成する練習

--- main.go
+++ main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/google/uuid"
 )
 
 func main() {
-	fmt.Println()
+	uuidObj, _ := uuid.NewUUID()
+	fmt.Println("", uuidObj.String())
+
+	uuidObj, _ = uuid.NewRandom()
+	fmt.Println("", uuidObj.String())
 
 }
